bdf9a5094bfbad7b46a25d8057760d6b65a886a5
Andy Rossmeissl <andy@rossmeissl.net>
2010-04-05 11:41:19 -0400

    How to get listings back in JSON

---
+++ doc/NOTES.rb
@@ -0,0 +1,9 @@
+Process.exit
+
+ZWS_ID = 'X1-ZWz1c7urejqkgb_7x5fi'
+
+# Retrieve recent for-sale listsings for Atlanta, GA in JSON:
+'http://www.zillow.com/webservice/FMRWidget.htm?zws-id=X1-ZWz1c7urejqkgb_7x5fi&region=Atlanta+GA&status=forSale&status=makeMeMove&ranking=daysOnZillow&widget=fmrwidget_nfs&output=json'
+
+# Retrieve recently-sold listings for Atlanta, GA in JSON:
+# just use 'rsh' instead of 'nfs'
