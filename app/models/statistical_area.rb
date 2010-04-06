fdba7c68f656f9e906d2e083dff8e89def21e99c
Andy Rossmeissl <andy@rossmeissl.net>
2010-04-06 07:29:25 -0400

    Sometimes listings don't have a room count; in these cases they come through as zero, but we should treat them as nils

--- app/models/statistical_area.rb
+++ app/models/statistical_area.rb
@@ -18,8 +18,8 @@ class StatisticalArea < ActiveRecord::Base
       next if %w(lot multiFamily).include? result['homeType']
       listing = listings.find_or_create_by_zpid( Listing.zpid_from_url(result['detailPageLink']) ).update_attributes(
                                 :zipcode => result['address']['zipcode'],
-                                :bathrooms => result['bathrooms'].to_f,
-                                :bedrooms => result['bedrooms'].to_i,
+                                :bathrooms => result['bathrooms'].to_f.nonzero?,
+                                :bedrooms => result['bedrooms'].to_i.nonzero?,
                                 :zillow_home_type => (result['homeType'] == 'unknown' ? nil : result['homeType']),
                                 :floorspace => result['finishedSqFt'].to_i.nonzero? )
       #listing.calculate_emission!
