ad05347e6eedf0883c740db30340a3511aa92db5
Andy Rossmeissl <andy@brighterplanet.com>
2010-04-27 16:03:24 -0400

    pages list

--- doc/NOTES.rb
+++ doc/NOTES.rb
@@ -7,3 +7,37 @@ ZWS_ID = 'X1-ZWz1c7urejqkgb_7x5fi'
 
 # Retrieve recently-sold listings for Atlanta, GA in JSON:
 # just use 'rsh' instead of 'nfs'
+
+=begin
+PAGES
+
+homepage aka statistical_areas/index
+------------------------------------
+
+rankings table
+  - place #
+  - name (link to zillow listings)
+  - avg annual footprint
+  - time series footprint sparkline
+  - (later: avg monthly energy cost)
+  
+overlaid time series line graphs
+  - avg. home footprint: line for each SA
+  - (later: avg monthly energy cost)
+  
+middleware promotional footer
+
+
+later: statistical_areas/show
+----------------------
+
+overlaid time series line graphs
+table of listings used
+
+later: listings/show
+----------------------
+
+- big button to zillow listing
+- attributes as parsed
+
+=end
