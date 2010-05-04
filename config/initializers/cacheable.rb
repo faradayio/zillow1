13cd7e0284210232dd63889cc44ef3332bc12d58
Seamus Abshere <seamus@abshere.net>
2010-05-03 22:01:29 -0400

    Try using heroku's memcache settings

--- config/initializers/cacheable.rb
+++ config/initializers/cacheable.rb
@@ -1,9 +1,11 @@
-servers = [ 'localhost:11211' ]
+# http://github.com/tfe/heroku_memcache/blob/master/init.rb
+servers = ENV['MEMCACHE_SERVERS'].split(',')
 options = {
   :support_cas => true,
   :binary_protocol => true,             # Use the binary protocol to reduce query processing overhead. Defaults to false.
   :connect_timeout => 5,                # How long to wait for a connection to a server. Defaults to 2 seconds. Set to 0 if you want to wait forever.
   :timeout => 0.25,                     # How long to wait for a response from the server. Defaults to 0.25 seconds. Set to 0 if you want to wait forever.
-  :server_failure_limit => 25           # How many consecutive failures to allow before marking a host as dead. Has no effect unless :retry_timeout is also set. Defaults to 2
+  :server_failure_limit => 25,          # How many consecutive failures to allow before marking a host as dead. Has no effect unless :retry_timeout is also set. Defaults to 2
+  :namespace => ENV['MEMCACHE_NAMESPACE']
 }
 Cacheable.repository = Memcached.new servers, options
