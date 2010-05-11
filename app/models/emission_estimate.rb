4a142d5249af7192f7fc6f4944d57ad2a754dce6
Andy Rossmeissl <andy@rossmeissl.net>
2010-05-11 18:14:09 -0400

    Include bathroom count in estimate requests

--- app/models/emission_estimate.rb
+++ app/models/emission_estimate.rb
@@ -3,7 +3,7 @@ class EmissionEstimate < Weary::Base
   post 'emission' do |resource|
     resource.url = 'http://carbon.brighterplanet.com/residences.json'
     resource.requires = [ :'residence[zip_code][name]' ]
-    resource.with = [ :'residence[floorspace_estimate]', :'residence[residence_class][name]', :'residence[bedrooms]' ]
+    resource.with = [ :'residence[floorspace_estimate]', :'residence[residence_class][name]', :'residence[bedrooms]', :'residence[bathrooms]' ]
   end
 
   class << self
@@ -11,6 +11,7 @@ class EmissionEstimate < Weary::Base
       new.emission( :'residence[zip_code][name]' => listing.zipcode,
                     :'residence[floorspace_estimate]' => listing.floorspace.square_feet.to(:square_meters),
                     :'residence[bedrooms]' => listing.bedrooms,
+                    :'residence[bathrooms]' => listing.bathrooms,
                     :'residence[residence_class][name]' => listing.residence_class ).perform.parse['emission']
     end
   end
