36d4b2356299a0d0675579971d9ecffac81a7eb7
Derek Kastner <dkastner@gmail.com>
2011-07-26 17:14:22 -0400

    Cache everything for 50 hours to overlap nicely w/cron

--- config/initializers/caching.rb
+++ config/initializers/caching.rb
@@ -1,3 +1,4 @@
 # sabshere 2/21/11 see config/environment.rb for Rails.cache setup
 
 CacheMethod.config.storage = Rails.cache # Dalli::Client.new
+CacheMethod.config.default_ttl = 180_000 # 50 hours
