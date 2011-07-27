d8643a6baf61642365b5b580d0469b5c99879d51
Seamus Abshere <seamus@abshere.net>
2011-07-26 19:13:47 -0500

    fix up indexes

--- db/migrate/20110713183411_add_indexes.rb
+++ db/migrate/20110713183411_add_indexes.rb
@@ -1,10 +1,15 @@
 class AddIndexes < ActiveRecord::Migration
   def self.up
     add_index :appearances, :listing_id
-    add_index :listings, :zpid
+    add_index :appearances, :appeared_at
+    add_index :listings, :zpid, :unique => true
     add_index :listings, :statistical_area_id
   end
 
   def self.down
+    remove_index :appearances, :listing_id
+    remove_index :appearances, :appeared_at
+    remove_index :listings, :zpid
+    remove_index :listings, :statistical_area_id
   end
 end
