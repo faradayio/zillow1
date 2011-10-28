1c29e26aea928c7421a69696ca23e31988768ab7
Derek Kastner <dkastner@gmail.com>
2011-10-28 10:45:43 -0400

    Don't convert nils to 0s

--- app/models/statistical_area.rb
+++ app/models/statistical_area.rb
@@ -71,7 +71,7 @@ class StatisticalArea < ActiveRecord::Base
         appearances.appeared_at BETWEEN '#{day.to_time.to_formatted_s(:db)}' AND '#{day.to_time.tomorrow.to_formatted_s(:db)}' AND
         listings.emission IS NOT NULL
     }
-    connection.select_value(sql).to_f
+    connection.select_value(sql)
   end
   cache_method :average_emission, 50.hours
   
