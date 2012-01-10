108306b73f06cc2d747e892a5b166d4e799d4902
Derek Kastner <dkastner@gmail.com>
2012-01-10 16:44:03 -0500

    Fix status route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Zillow1::Application.routes.draw do
-  resources :statistical_areas, :collection => { :status => :get }
+  resources :statistical_areas do
+    collection do
+      get :status
+    end
+  end
   root :to => 'statistical_areas#index'
 end
